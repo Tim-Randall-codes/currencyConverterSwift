e1320f1d607760c42e481f5b78a8c28629edd8fd
Tim Randall <trtimrandall@gmail.com>
2021-05-20 22:57:52 +1000

    Initial Commit

---
+++ CurrencyConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyConvert
+//
+//  Created by Tim Randall on 20/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyConvert/CurrencyConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertApp.swift
+//  CurrencyConvert
+//
+//  Created by Tim Randall on 20/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
