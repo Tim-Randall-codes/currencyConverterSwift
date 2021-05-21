adea9082f2b7bdaa9addd1b1e1da8dadb31eb50a
Tim Randall <trtimrandall@gmail.com>
2021-05-21 21:59:04 +1000

    final commit

--- CurrencyConvert/ContentView.swift
+++ CurrencyConvert/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var aud: String = "0"
+    @State var usd: Double = 0
+    @State var rmb: Double = 0
+    @State var euro: Double = 0
+    @State var jpy: Double = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Currency Converter!")
+                .padding()
+            Text("Accurate on 21/5/2021")
+                .padding()
+            TextField("Australian dollars", text: $aud)
+                .keyboardType(.numberPad)
+                .padding()
+                .border(Color.black, width: 2)
+            Button("Convert", action:{
+                let iaud = Int(aud) ?? 0
+                usd = Double(iaud) * 0.78
+                rmb = Double(iaud) * 5
+                euro = Double(iaud) * 0.68
+                jpy = Double(iaud) * 84.46
+            }).padding().border(Color.pink, width: 3)
+            
+            Spacer()
+            HStack{
+                Text("USD")
+                    .padding()
+                Text(String(usd))
+                    .padding()
+            }
+            HStack{
+                Text("RMB")
+                    .padding()
+                Text(String(rmb))
+                    .padding()
+            }
+            HStack{
+                Text("Euro")
+                    .padding()
+                Text(String(euro))
+                    .padding()
+            }
+            HStack{
+                Text("JPY")
+                    .padding()
+                Text(String(jpy))
+                    .padding()
+            }
+        }
     }
 }
 
